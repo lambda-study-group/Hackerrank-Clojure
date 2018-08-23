2465374d75e8afe9d6198d0c36b78d8367b3a225
Giuliano Lauro <giulianolauro@gmail.com>
2018-08-23 16:35:55 -0300

    Improve mini-max-sim

--- problem-solving/mini-max-sum.clj
+++ problem-solving/mini-max-sum.clj
@@ -1,13 +1,30 @@
 (defn slice
-  ([coll start] (subvec (vec coll) start))
-  ([coll start end] (subvec (vec coll) start end)))
+  ([coll start] (drop start coll))
+  ([coll start end]
+   (let [len (count coll)
+         pos-index #(if (neg? %)
+                      (max (- len %) 0)
+                      %)
+         start (pos-index start)
+         end (pos-index end)]
+     (->> coll (drop start) (take (- end start))))))
 
+(slice [86 86] 0 2)
 (defn minimum [coll n]
   (apply + (slice (sort coll) 0 n)))
 
 (defn maximum [coll n]
-  (let [last-i (count coll)]
-    (apply + (slice (sort coll) (- last-i n)))))
+  (apply + (slice (sort coll) (- n))))
 
-(defn miniMaxSum [coll]
-  (print (str (minimum coll 4) " " (maximum coll 4))))
+(defn miniMaxSum [coll n]
+  (str (minimum coll n) " " (maximum coll n)))
+
+;; Testing cases
+
+(defn nums [n]
+  (take n (repeatedly #(rand-int 100))))
+
+(def arr (nums 2))
+(list* arr)
+
+(print (miniMaxSum arr 2))
